272ba44a5dbb16c47f004e829176af99235e2f3e
DejaLFlynn <dejaflynn@pursuit.org>
2020-06-25 16:59:42 -0400

    sign up and authentication additions

--- imstagramFinal/final/frontend/src/Components/Authentication.js/Authentication.js
+++ imstagramFinal/final/frontend/src/Components/Authentication.js/Authentication.js
@@ -1,9 +1,10 @@
 import React, {useState} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect} from 'react-router-dom';
 import NavBar from './NavBar'
 import LandingPage from './LandingPage'
-
-
+import Explore from './ExplorePage'
+import User from './User'
+import SignUp from '../SignUp'
 const Authentication = () => {
     
     const [authentication, setAuthentication]  = useState(false) 
@@ -16,10 +17,32 @@ const Authentication = () => {
     return (
         
         <>
-        <div>
-<LandingPage/>
-        <NavBar/>    
-        </div>
+   
+   {!authentication ?
+                <Switch>
+                <Route exact path={"/"}>
+                    <LandingPage onLogin={handleAuthentication}/>
+                </Route>
+                <Route exact path={"/signUp"}>
+                    <SignUp onLogin={handleAuthentication}/>
+                </Route>
+                </Switch>
+                : <Redirect to={"/explore"}/> }
+
+            {authentication ?
+                <>
+                <NavBar />
+                <Switch>
+                <Route exact path={"/explore"} >
+                    <Explore/>
+                </Route>
+                <Route exact path={"/user"}>
+                    <User />
+                </Route>
+
+                </Switch>
+                </>
+            : <Redirect to={"/"}/>  }
             
         </>
     )
---
+++ imstagramFinal/final/frontend/src/Components/SignUp.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import { useInput } from "../Utils/Input";
+import axios from "axios";
+import Input from "./Authentication.js/Input";
+
+const SignUpPage = ({onLogin, modalClose }) => {
+  const username = useInput("");
+  const email = useInput("");
+  const fullname = useInput("");
+  const age = useInput("");
+  const profile_pic = useState("");
+
+  const [image, setImage] = useState(null);
+
+  const [error, setError] = useState("");
+
+
+  const handleUpload = event => {
+    setImage(event.target.files[0]);
+  };
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("image", image);
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      };
+      let res = await axios.post(
+        "http://localhost:4000/upload",
+        formData,
+        config
+      );
+      let imageUrl = res.data.imageUrl;
+      let newUser = await axios.post("http://localhost:4000/users", {
+        email: email.value,
+        fullname: fullname.value,
+        username: username.value,
+        age: age.value,
+        profile_pic: imageUrl
+      });
+
+      if (newUser) {
+        sessionStorage.setItem("username", username.value);
+        sessionStorage.setItem("id", newUser.data.payload.id);
+        modalClose()
+        onLogin();
+      }
+    } catch (error) {
+      setError("Invalid Input: Username and/or Email Exists");
+    }
+  };
+
+  return (
+    <>
+      <div className="signUpFormContainer">
+        <form className="signUpForm" onSubmit={handleSubmit}>
+          <div className="inputContainer">
+            <Input
+              className="input"
+              type={"text"}
+              name={"username"}
+              placeholder={"Pick a Username"}
+              input={username}
+            />
+          </div>
+
+          <div className="inputContainer">
+            <Input
+              className="input"
+              type={"text"}
+              name={"email"}
+              placeholder={"Email"}
+              input={email}
+            />
+          </div>
+
+          <div className="inputContainer">
+            <Input
+              className="input"
+              type={"text"}
+              name={"fullname"}
+              placeholder={"Full Name"}
+              input={fullname}
+            />
+          </div>
+
+          <div className="inputContainer">
+            <Input
+              className="input"
+              type={"number"}
+              name={"age"}
+              placeholder={"Age"}
+              input={age}
+            />
+          </div>
+
+          <div className="uploadImage">
+            <label>
+              {" "}
+              Upload Profile Pic:
+              <input
+                className="uploadImage"
+                type={"file"}
+                name={"profilePic"}
+                onChange={event => handleUpload(event)}
+              />
+            </label>
+          </div>
+
+          <button className="signUpButton" type="submit">
+            Login
+          </button>
+        </form>
+
+        {/* <div>{ !error ? <Error message={error} /> : null}</div> */}
+      </div>
+    </>
+  );
+};
+export default SignUpPage;
