74826efeb4df73b9114d9de7bc8ba0f3b7d8c14c
DejaLFlynn <dejaflynn@pursuit.org>
2020-07-12 12:27:19 -0400

    uploading component

--- imstagramFinal/final/frontend/src/Components/Upload.js
+++ imstagramFinal/final/frontend/src/Components/Upload.js
@@ -1,15 +1,57 @@
-import React, {useState}from 'react';
-import axios from 'axios'
+import React, { useState, useRef, useEffect } from "react";
+import ReactDOM from "react-dom";
 
 const Upload =()=>{
 
+    const [image, setImage] = useState({ preview: "", raw: "" });
 
-    return(
-        <div className="upload">
-            upload Component
-            <input type="file" />
-            <button ></button>
-        </div>
-    )
+    const handleChange = e => {
+      if (e.target.files.length) {
+        setImage({
+          preview: URL.createObjectURL(e.target.files[0]),
+          raw: e.target.files[0]
+        });
+      }
+    };
+  
+    const handleUpload = async e => {
+      e.preventDefault();
+      const formData = new FormData();
+      formData.append("image", image.raw);
+  
+      await fetch("YOUR_URL", {
+        method: "POST",
+        headers: {
+          "Content-Type": "multipart/form-data"
+        },
+        body: formData
+      });
+    };
+  
+    return (
+      <div>
+        <label htmlFor="upload-button">
+          {image.preview ? (
+            <img src={image.preview} alt="holder" width="300" height="300" />
+          ) : (
+            <>
+              <span className="stack">
+                <i className="circle" />
+                <i className="store" />
+              </span>
+              <h5 className="text">Upload your photo</h5>
+            </>
+          )}
+        </label>
+        <input
+          type="file"
+          id="upload-button"
+          style={{ display: "none" }}
+          onChange={handleChange}
+        />
+        <br />
+        <button onClick={handleUpload}>Upload</button>
+      </div>
+    );
 };
 export default Upload;
