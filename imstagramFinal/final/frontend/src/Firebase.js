58b84dc7643e349f72427961c68fd92396e7fbac
DejaLFlynn <dejaflynn@pursuit.org>
2020-07-17 16:05:58 -0400

    updated firebase info

--- imstagramFinal/final/frontend/src/Firebase.js
+++ imstagramFinal/final/frontend/src/Firebase.js
@@ -1,25 +1,62 @@
 import app from 'firebase/app'
 import 'firebase/auth'
-const {
-  REACT_APP_APIKEY,
-  REACT_APP_AUTHDOMAIN,
-  REACT_APP_DATABASEURL,
-  REACT_APP_PROJECTED,
-  REACT_APP_STORAGEBUCKET,
-  REACT_APP_MESSAGINGSENDERID,
-  REACT_APP_APPID
-  
-    
-} = process.env;
+import 'firebase/firebase-firestore'
 
 const config = {
-    apiKey: REACT_APP_APIKEY,
-    authDomain: REACT_APP_AUTHDOMAIN,
-    databaseURL: REACT_APP_DATABASEURL,
-    projectId: REACT_APP_PROJECTED,
-    storageBucket: REACT_APP_STORAGEBUCKET,
-    messagingSenderId: REACT_APP_MESSAGINGSENDERID,
-    appId: REACT_APP_APPID
-  };
-app.initializeApp(config)
-export default app;
+	apiKey: "AIzaSyCvjxM7NCh880Xprd2AWB0aTholZ0nzt4s",
+	authDomain: "imstagram-cta.firebaseapp.com",
+	databaseURL: "https://imstagram-cta.firebaseio.com/",
+	projectId: "imstagram-cta",
+	storageBucket: "gs://imstagram-cta.appspot.com",
+	messagingSenderId: "902733136206"
+}
+
+class Firebase {
+	constructor() {
+		app.initializeApp(config)
+		this.auth = app.auth()
+		this.db = app.firestore()
+	}
+
+	login(email, password) {
+		return this.auth.signInWithEmailAndPassword(email, password)
+	}
+
+	logout() {
+		return this.auth.signOut()
+	}
+
+	async register(name, email, password) {
+		await this.auth.createUserWithEmailAndPassword(email, password)
+		return this.auth.currentUser.updateProfile({
+			displayName: name
+		})
+	}
+
+	addQuote(quote) {
+		if(!this.auth.currentUser) {
+			return alert('Not authorized')
+		}
+
+		return this.db.doc(`users_codedamn_video/${this.auth.currentUser.uid}`).set({
+			quote
+		})
+	}
+
+	isInitialized() {
+		return new Promise(resolve => {
+			this.auth.onAuthStateChanged(resolve)
+		})
+	}
+
+	getCurrentUsername() {
+		return this.auth.currentUser && this.auth.currentUser.displayName
+	}
+
+	async getCurrentUserQuote() {
+		const quote = await this.db.doc(`users_codedamn_video/${this.auth.currentUser.uid}`).get()
+		return quote.get('quote')
+	}
+}
+
+export default new Firebase()
