ff91ef545284ecd580553124d37ea9bcd517cfea
DejaLFlynn <dejaflynn@pursuit.org>
2020-08-01 14:02:47 -0400

    updated home component

--- imstagramFinal/final/frontend/src/App.js
+++ imstagramFinal/final/frontend/src/App.js
@@ -6,13 +6,14 @@ import NavBar from './Components/Authentication.js/NavBar'
 import Explorer from './Components/Explorer'
 import { Route, Switch } from 'react-router-dom'
 import Error from './Components/Error'
+import Home from './Components/Home'
 function App() {
  
   return (
     <div className ="App">
       <NavBar/>
       <Switch>
-    <Route exact path={'/'}>Home</Route>
+    <Route exact path={'/'}><Home/></Route>
     <Route exact path={'/posts'}> <Explorer/></Route>
     <Route path={'/users'}><User/> </Route>
     <Route path='*' component={Error}/>
