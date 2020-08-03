c8a7ceead0ba44e1746bdfc5f8d6313fcd35f8fb
DejaLFlynn <dejaflynn@pursuit.org>
2020-08-03 11:55:43 -0400

    displaying all posts on explorer page

--- imstagramFinal/final/frontend/src/Components/Explorer.js
+++ imstagramFinal/final/frontend/src/Components/Explorer.js
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { apiURL } from "../Utils/apiURL";
-
+import "../CSS/Explore.CSS";
 const Explorer = () => {
-  const [posts, setPosts] = useState('');
+  const [posts, setPosts] = useState([]);
   const API = apiURL();
 
   const fetchPosts = async () => {
-  try {
-    let res = await axios.get(`${API}/posts`);
-    setPosts(res.data.posts[0].posts_images)
-      
-  } catch (error) {
-      console.log(error)
-  }
-  }
- useEffect(()=>{
-     fetchPosts()
- },[])
-  return (<div className="Explorer">
-<img src={posts}>
-</img>
-  </div>
+    try {
+      let res = await axios.get(`${API}/posts`);
+      setPosts(res.data.posts);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+  
+  let displayPics = posts.map((post) => {
+    return (
+      <div className="allPics" key={post.id + post.posts_images + post.content}>
+    
+        <img src={post.posts_images}></img>
+        <p value={post.content}></p>
+      </div>
+    );
+  });
+  return (
+    <div className="Explorer">
+      <div className="pictureContainer">
+        <img src={posts}></img>
+        {displayPics}
+      </div>
+    </div>
   );
 };
 export default Explorer;
