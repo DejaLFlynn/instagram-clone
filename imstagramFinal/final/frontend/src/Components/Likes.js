ef2b055650c949fe24b077622acb66c4cb53bf19
DejaLFlynn <dejaflynn@pursuit.org>
2020-08-16 12:06:59 -0400

    frontend like component started

--- imstagramFinal/final/frontend/src/Components/Likes.js
+++ imstagramFinal/final/frontend/src/Components/Likes.js
@@ -1,1 +1,32 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import {apiURL} from '../Utils/apiURL'
+
+const Likes=({post_id})=>{
+    const [likes, setLikes] = useState([]);
+
+    const getLikes = async () => {
+        try {
+          const res = await getLikeCommentByPostId(post_id);
+          debugger
+          if (res) {
+            setLikes(res.length);
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+    
+      useEffect(() => {
+        getLikes()
+      }, []);
+    
+    return(
+        <div>
+
+
+        </div>
+    )
+
+}
+
+export default Likes;
