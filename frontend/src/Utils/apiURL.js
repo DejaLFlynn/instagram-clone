74c9c5f758071d4bdbc3b2d3d42a64f8f942ee79
DejaLFlynn <dejaflynn@pursuit.org>
2020-09-26 12:46:20 -0400

    fixing heroku

--- frontend/src/Utils/apiURL.js
+++ frontend/src/Utils/apiURL.js
@@ -1,4 +1,4 @@
 export const apiURL = () =>{
   return window.location.hostname === "localhost" ?
-  "http://localhost:4001" : "https://imstagram-final.herokuapp.com/";
+  "http://localhost:4001" : "https://cta-final.herokuapp.com/";
 }
