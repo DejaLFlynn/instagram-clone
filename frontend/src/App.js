297fae3a0ef3c2ef034a5e7a47a029305f4e377a
DejaLFlynn <dejaflynn@pursuit.org>
2020-10-19 11:16:03 -0400

    added package and started material styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,19 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./Components/Home";
 import Context from "./Providers/Context";
 import { AuthRoute, ProtectedRoute } from "./Utils/Route";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 function App() {
   return (
     <div className="App">
