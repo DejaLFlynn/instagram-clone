5e3ba58e4833f9110a7749303da7a63c1740df7b
DejaLFlynn <dejaflynn@pursuit.org>
2020-12-01 16:11:11 -0500

    removed paper form home

--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -2,25 +2,57 @@ import React from "react";
 import { useHistory, Link } from "react-router-dom";
 import { login } from "../Utils/Firebase";
 import { apiURL } from "../Utils/apiURL";
-import "../CSS/Home.css";
+// import "../CSS/Home.css";
 import SignIn from "./Authentication.js/SignIn";
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(5),
+    textAlign: 'justify',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 const API = apiURL();
 const Home = () => {
+  const classes = useStyles();
+
   const history = useHistory();
 
   return (
     <>
       <div className="div">
-        <div className="rightSide">
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+   
+        <Grid item xs={6}>
+        
+          <div className="rightSide">
           <img
             src="https://i.postimg.cc/RhDKs1vV/igphones.png"
             alt="iphone"
-            width="450px"
+            width="400px"
           ></img>
         </div>
-        <div className="leftSide">
+         
+        </Grid>
+        <Grid item xs={6}>
+        
+          
           <SignIn />
-        </div>
+        
+         
+        </Grid>
+  
+      </Grid>
+    </div>
+     
+
       </div>
     </>
   );
