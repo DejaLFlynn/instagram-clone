90c6b4b041815c79df55595220470846a0e3c1cb
DejaLFlynn <dejaflynn@pursuit.org>
2020-12-01 14:55:46 -0500

    started styling home page with exact images and cards for login

--- frontend/src/Components/Authentication.js/SignIn.js
+++ frontend/src/Components/Authentication.js/SignIn.js
@@ -3,9 +3,28 @@ import { Link, withRouter } from "react-router-dom";
 import { useHistory } from "react-router-dom";
 import { login } from "./../../Utils/Firebase";
 import { apiURL } from "../../Utils/apiURL";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+
 const API = apiURL();
 
 const SignIn = () => {
+  const classes = useStyles();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const history = useHistory();
@@ -20,32 +39,73 @@ const SignIn = () => {
       console.log(error);
     }
   };
+  const handleGuestSignIn = async () => {
+    await login("guest", "account");
+    history.push("/posts");
+  };
   return (
     <div className="SignIn">
-      <form onSubmit={handleSubmit}>
-        <input
-          autoComplete="on"
-          type="text"
-          className="email"
-          value={email}
-          placeholder="email"
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        ></input>
-        <input
-          autoComplete="on"
-          type="password"
-          className="password"
-          value={password}
-          placeholder="password"
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        ></input>
-        <input type="submit"></input>
-      </form>
+      <Card className={classes.root}>
+        <CardActionArea>
+          <CardContent>
+            <img
+              src="https://i.postimg.cc/44bfJzn7/Screen-Shot-2020-12-01-at-9-58-32-AM.png"
+              width="250"
+              height="100px"
+            />
+          </CardContent>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <input
+                autoComplete="on"
+                type="text"
+                className="email"
+                value={email}
+                placeholder="email"
+                onChange={(e) => setEmail(e.currentTarget.value)}
+              ></input>
+              <input
+                autoComplete="on"
+                type="password"
+                className="password"
+                value={password}
+                placeholder="password"
+                onChange={(e) => setPassword(e.currentTarget.value)}
+              ></input>
+              <input type="submit"></input>
+            </form>
+          </CardContent>
+        </CardActionArea>
+        <CardActions></CardActions>
+      </Card>
+ 
       <div>
+      <Card className={classes.root}>
+    <CardActionArea>
+
         <Link to="/signup" className="signUp">
           Don't have an account?
           <span className="span"> Sign up </span>
         </Link>
+        <button className="guest" onClick={handleGuestSignIn}>
+              Guest
+            </button>
+         
+
+    </CardActionArea>
+        </Card>
+        <br></br>
+        <p>Get the App</p>
+          <img
+            className="appleStore"
+            src="https://losserranosgolfclub.com/wp-content/uploads/Download-on-the-App-Store-button.png"
+            alt="appleStore" width="100px"
+          ></img>
+          <img
+            className="android"
+            src="https://www.fcsok.org/wp-content/uploads/2020/04/get-it-on-google-play-badge.png"
+            alt="googlePlay" width="120px"
+          ></img>
       </div>
     </div>
   );
--- frontend/src/Components/Home.js
+++ frontend/src/Components/Home.js
@@ -7,48 +7,19 @@ import SignIn from "./Authentication.js/SignIn";
 const API = apiURL();
 const Home = () => {
   const history = useHistory();
-  const handleGuestSignIn = async () => {
-    await login("guest", "account");
-    history.push("/posts");
-  };
+
   return (
     <>
       <div className="div">
         <div className="rightSide">
-          {/* <img src="https://i.pinimg.com/originals/9a/f7/af/9af7af3a1232ad180a427b86eb84d4f4.png"
-          width="300px"/>  */}
           <img
-            src="https://media.giphy.com/media/hsDY1IPzpP4DcB1Ba5/giphy.gif"
+            src="https://i.postimg.cc/RhDKs1vV/igphones.png"
             alt="iphone"
+            width="450px"
           ></img>
-           </div>
-           <div className="leftSide">
-
-          
-         
-       
-         
-            <SignIn />
-            {/* <p>
-              {" "}
-              ** Please use "guest@gmail.com" and "account" for password while
-              button is worked on!
-            </p> */}
-            <button className="guest" onClick={handleGuestSignIn}>
-              Guest
-            </button>
-         
-          <p>Get the App</p>
-          <img
-            className="appleStore"
-            src="https://losserranosgolfclub.com/wp-content/uploads/Download-on-the-App-Store-button.png"
-            alt="appleStore" width="100px"
-          ></img>
-          <img
-            className="android"
-            src="https://www.fcsok.org/wp-content/uploads/2020/04/get-it-on-google-play-badge.png"
-            alt="googlePlay" width="120px"
-          ></img>
+        </div>
+        <div className="leftSide">
+          <SignIn />
         </div>
       </div>
     </>
