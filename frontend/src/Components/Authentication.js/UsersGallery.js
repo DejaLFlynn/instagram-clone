5504ea4adb8f152aaea42fc891f8afe54a85f073
DejaLFlynn <dejaflynn@pursuit.org>
2020-12-08 13:13:36 -0500

    mapping through post images

--- frontend/src/Components/Authentication.js/UsersGallery.js
+++ frontend/src/Components/Authentication.js/UsersGallery.js
@@ -5,13 +5,57 @@ import axios from "axios";
 import Upload from "../Upload";
 import { useParams } from "react-router-dom";
 import NavBar from "./NavBar";
-
+import ShowComments from "../Comments/ShowComments";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import IconButton from "@material-ui/core/IconButton";
 // import { makeStyles } from "@material-ui/core/styles";
-
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 const UsersGallery = () => {
+  const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   const API = apiURL();
   const { currentUsers, token } = useContext(AContext);
   const [posts, setPosts] = useState([]);
+  const classes = useStyles();
+
   const fetchUserById = async () => {
     try {
       let res = await axios({
@@ -21,9 +65,8 @@ const UsersGallery = () => {
           AuthToken: token,
         },
       });
-debugger
+      debugger
       setPosts(res.data.payload);
-     
     } catch (err) {
       console.log(err);
     }
@@ -35,14 +78,22 @@ debugger
   useEffect(() => {
     fetchUserById();
   }, [posts, token]);
+//   let display = posts.map((post, index)=>{
+//     return(
+//         <div key={index} className="contentDisplay">
+         
+//     <p>{post.posts_images}</p>
+
+//         </div>
+//     )
+// })
 
   return (
-  <div className="UsersGallery">
-
-
-      Users Posts
-  </div>
-  )
+    <div className="UsersGallery">
+        {/* {display} */}
+     
+    </div>
+  );
 };
 
 export default UsersGallery;
