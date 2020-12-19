3840b2854fdc93f26b807d40a745ed42b8e63ee3
DejaLFlynn <dejaflynn@pursuit.org>
2020-12-19 17:55:17 -0500

    add name to payload in query instead of just having one call

--- backend/queries/users/posts/posts.js
+++ backend/queries/users/posts/posts.js
@@ -2,64 +2,72 @@ const db = require("../../../db/index");
 
 const createPost = async (req, res, next) => {
   try {
-      console.log(req.body)
-       let newPost =  await db.one('INSERT INTO posts(id, user_id, posts_images, content) VALUES(${id}, ${user_id}, ${posts_images}, ${content}) RETURNING *', req.body);
-        res.status(200).json({
-          status: 'success',
-          message: 'a new post was created',
-          body: newPost
-  
-        });
-      } catch (err) {
-        next(err);
-      }
+    console.log(req.body);
+    let newPost = await db.one(
+      "INSERT INTO posts(id, user_id, posts_images, content) VALUES(${id}, ${user_id}, ${posts_images}, ${content}) RETURNING *",
+      req.body
+    );
+    res.status(200).json({
+      status: "success",
+      message: "a new post was created",
+      body: newPost,
+    });
+  } catch (err) {
+    next(err);
+  }
 };
 const allPosts = async (req, res, next) => {
-    try {
-        const posts = await db.any('SELECT * FROM posts');
-        res.json({
-            posts,
-            message: "All Posts"
-        })
-    } catch (err) {
-        next(err);
-    }
-}
-const deletePost = async (req, res, next) => {
-    try {
-      await db.none('DELETE FROM posts WHERE id = $1 RETURNING *', req.params.id);
-      res.status(200).json({
-        status: "success",
-        message: "The post is deleted"
-      });
-    } catch (err) {
-        next(err);
-    }
-  };
-
-  const getPost = async (req, res, next) => {
-    try {
-      let posts = await db.any("SELECT * FROM posts");
-      res.json({
-          status: "Success",
-          payload: {posts},
-          message: "All POSTS"
-      });
-    } catch (err) {
-        next(err);
-    }
-  };
-  const fetchAllForOne = async(req, res, next)=>{
-    try {
-     
-      let usersPosts = await db.any('SELECT posts.id, posts.user_id, posts.posts_images, posts.content, users.name FROM posts LEFT JOIN users ON posts.user_id=users.id WHERE user_id = $1', [req.params.user_id]);
-      res.json({
-        usersPosts,
-        message: "All posts for username"
-      })
-    } catch (error) {
-      next(err)
-    }
+  try {
+    const posts = await db.any("SELECT * FROM posts");
+    res.json({
+      posts,
+      message: "All Posts",
+    });
+  } catch (err) {
+    next(err);
   }
-module.exports = { createPost, allPosts, deletePost , getPost, fetchAllForOne
 };
+const deletePost = async (req, res, next) => {
+  try {
+    await db.none("DELETE FROM posts WHERE id = $1 RETURNING *", req.params.id);
+    res.status(200).json({
+      status: "success",
+      message: "The post is deleted",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+const getPost = async (req, res, next) => {
+  try {
+    let posts = await db.any("SELECT * FROM posts");
+    res.json({
+      status: "Success",
+      payload: { posts },
+      message: "All POSTS",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+const fetchAllForOne = async (req, res, next) => {
+  try {
+    let usersName = await db.any(
+      "SELECT posts.id, posts.user_id, posts.posts_images, posts.content, users.name FROM posts LEFT JOIN users ON posts.user_id=users.id WHERE users.name = $1",
+      [req.params.name]
+    );
+    let usersPosts = await db.any(
+      "SELECT posts.id, posts.user_id, posts.posts_images, posts.content, users.name FROM posts LEFT JOIN users ON posts.user_id=users.id WHERE user_id = $1",
+      [req.params.user_id]
+    );
+    res.json({
+      usersPosts,
+      usersName,
+      message: "All posts for username",
+    });
+  } catch (error) {
+    next(err);
+  }
+};
+module.exports = { createPost, allPosts, deletePost, getPost, fetchAllForOne };
