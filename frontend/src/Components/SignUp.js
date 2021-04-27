5467ce0f846162aa7140a201d00833bf4d6ea71e
DejaLFlynn <dejaflynn@pursuit.org>
2021-04-27 10:44:26 -0400

    fixed error posts new user

--- frontend/src/Components/SignUp.js
+++ frontend/src/Components/SignUp.js
@@ -5,49 +5,37 @@ import { apiURL } from "../Utils/apiURL";
 import { signUp } from "../Utils/Firebase";
 import { AContext } from "../Providers/Context";
 import { storage } from "../Firebase";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 
-import Avatar from "@material-ui/core/Avatar";
-import Button from "@material-ui/core/Button";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import Box from "@material-ui/core/Box";
-import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
-import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-// //component signs up user using name, email, password and picture
-// //sends data to firebase
-// //qurans@gmail.com
-// //password
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      <Link color="inherit" href="https://material-ui.com/">
-        https://cta-imstagram.netlify.app/
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
+//component signs up user using name, email, password and picture
+//sends data to firebase
+//qurans@gmail.com
+//password
 const useStyles = makeStyles((theme) => ({
   paper: {
     marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   avatar: {
     margin: theme.spacing(1),
     backgroundColor: theme.palette.secondary.main,
   },
   form: {
-    width: "100%", // Fix IE 11 issue.
+    width: '100%', // Fix IE 11 issue.
     marginTop: theme.spacing(3),
   },
   submit: {
@@ -55,14 +43,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function SignUpPage() {
+const SignUpPage = () => {
   const classes = useStyles();
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const { token } = useContext(AContext);
   const [url, setUrl] = useState("");
-  const [bio, setBio] = useState("");
+  const [bio, setBio] =useState("");
   const [image, setImage] = useState(null);
   const [error, setError] = useState(null);
   const API = apiURL();
@@ -72,8 +60,7 @@ export default function SignUpPage() {
     e.preventDefault();
     try {
       let res = await signUp(email, password);
-      // debugger
-
+     
       let res2 = await axios({
         method: "post",
         url: `${API}/users`,
@@ -83,7 +70,7 @@ export default function SignUpPage() {
           AuthToken: token,
         },
       });
-
+      
       console.log(res2.data);
       history.push("/posts");
     } catch (error) {
@@ -117,100 +104,137 @@ export default function SignUpPage() {
       }
     );
   };
+
   return (
-    <Container component="main" maxWidth="xs">
+    <>
+      <div>
+      <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
+    
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} noValidate>
+        <form className={classes.form} onSubmit={handleSubmit} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
-                required
-                fullWidth
-                id="firstName"
-                label="Username"
+                autoComplete="name"
                 value={name}
                 onChange={(e) => setName(e.currentTarget.value)}
-                autoFocus
+                required
+                fullWidth
+                id="Username" label="Username" variant="filled" 
               />
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
-                variant="outlined"
-                required
-                fullWidth
-                id="lastName"
-                label="Bio"
-                name="lastName"
-                autoComplete="lname"
                 value={bio}
                 onChange={(e) => setBio(e.currentTarget.value)}
+                required
+                fullWidth
+                id="bio"
+                label="Bio"
+                name="bio"
+                autoComplete="lname"
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.currentTarget.value)}
                 required
                 fullWidth
                 id="email"
                 label="Email Address"
                 name="email"
                 autoComplete="email"
-                value={email}
-                onChange={(e) => setEmail(e.currentTarget.value)}
               />
             </Grid>
             <Grid item xs={12}>
               <TextField
                 variant="outlined"
+                value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
                 required
                 fullWidth
                 name="password"
                 label="Password"
                 type="password"
                 id="password"
-                value={password}
-                onChange={(e) => setPassword(e.currentTarget.value)}
                 autoComplete="current-password"
               />
             </Grid>
             <Grid item xs={12}>
-              <input type="file" onChange={handleChange} />
-              <br />
+            <input type="file" onChange={handleChange} />
+          <br />
+          
+       
             </Grid>
           </Grid>
-
           <Button
             type="submit"
             fullWidth
             variant="contained"
             color="primary"
+            label="Log In"
             className={classes.submit}
+      
           >
             Sign Up
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
-                Already have an account? Sign in
-              </Link>
+           
             </Grid>
           </Grid>
         </form>
-        <Button onClick={handleUpload}>Upload Image</Button>
+            <button onClick={handleUpload}>Upload Image</button>
       </div>
       <Box mt={5}>
-        <Copyright />
+   
       </Box>
     </Container>
+        {/* {error ? <div>{error}</div> : null} */}
+        {/* <form onSubmit={handleSubmit}>
+          <input
+            value={name}
+            onChange={(e) => setName(e.currentTarget.value)}
+            type="text"
+            placeholder="name"
+          />
+           <input
+            value={bio}
+            onChange={(e) => setBio(e.currentTarget.value)}
+            type="text"
+            placeholder="bio"
+          />
+          <input
+            value={email}
+            onChange={(e) => setEmail(e.currentTarget.value)}
+            type="text"
+            placeholder="email"
+          />
+          <input
+            value={password}
+            onChange={(e) => setPassword(e.currentTarget.value)}
+            autoComplete="on"
+            type="password"
+            placeholder="password"
+          />
+          <input type="file" onChange={handleChange} />
+          <br />
+           <Dropzone handleImageChange={handleImageChange} dropzoneText={"Drop or Select Your Profile Image"}/> 
+          <button type="submit">Sign Up</button>
+        </form>
+        <button onClick={handleUpload}>Upload Image</button> */}
+      </div>
+    </>
   );
-}
+};
+
+export default SignUpPage;
 // import React, { useState, useContext } from "react";
 // import axios from "axios";
 // import { useHistory } from "react-router-dom";
@@ -219,33 +243,48 @@ export default function SignUpPage() {
 // import { AContext } from "../Providers/Context";
 // import { storage } from "../Firebase";
 
-// import Button from '@material-ui/core/Button';
-// import CssBaseline from '@material-ui/core/CssBaseline';
-// import TextField from '@material-ui/core/TextField';
-// import Grid from '@material-ui/core/Grid';
-// import Box from '@material-ui/core/Box';
+// import Avatar from "@material-ui/core/Avatar";
+// import Button from "@material-ui/core/Button";
+// import CssBaseline from "@material-ui/core/CssBaseline";
+// import TextField from "@material-ui/core/TextField";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Checkbox from "@material-ui/core/Checkbox";
+// import Link from "@material-ui/core/Link";
+// import Grid from "@material-ui/core/Grid";
+// import Box from "@material-ui/core/Box";
+// import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+// import Typography from "@material-ui/core/Typography";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Container from "@material-ui/core/Container";
+// // //component signs up user using name, email, password and picture
+// // //sends data to firebase
+// // //qurans@gmail.com
+// // //password
+// function Copyright() {
+//   return (
+//     <Typography variant="body2" color="textSecondary" align="center">
+//       <Link color="inherit" href="https://material-ui.com/">
+//         https://cta-imstagram.netlify.app/
+//       </Link>{" "}
+//       {new Date().getFullYear()}
+//       {"."}
+//     </Typography>
+//   );
+// }
 
-// import Typography from '@material-ui/core/Typography';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Container from '@material-ui/core/Container';
-
-// //component signs up user using name, email, password and picture
-// //sends data to firebase
-// //qurans@gmail.com
-// //password
 // const useStyles = makeStyles((theme) => ({
 //   paper: {
 //     marginTop: theme.spacing(8),
-//     display: 'flex',
-//     flexDirection: 'column',
-//     alignItems: 'center',
+//     display: "flex",
+//     flexDirection: "column",
+//     alignItems: "center",
 //   },
 //   avatar: {
 //     margin: theme.spacing(1),
 //     backgroundColor: theme.palette.secondary.main,
 //   },
 //   form: {
-//     width: '100%', // Fix IE 11 issue.
+//     width: "100%", // Fix IE 11 issue.
 //     marginTop: theme.spacing(3),
 //   },
 //   submit: {
@@ -253,14 +292,14 @@ export default function SignUpPage() {
 //   },
 // }));
 
-// const SignUpPage = () => {
+// export default function SignUpPage() {
 //   const classes = useStyles();
 //   const [email, setEmail] = useState("");
 //   const [name, setName] = useState("");
 //   const [password, setPassword] = useState("");
 //   const { token } = useContext(AContext);
 //   const [url, setUrl] = useState("");
-//   const [bio, setBio] =useState("");
+//   const [bio, setBio] = useState("");
 //   const [image, setImage] = useState(null);
 //   const [error, setError] = useState(null);
 //   const API = apiURL();
@@ -315,134 +354,99 @@ export default function SignUpPage() {
 //       }
 //     );
 //   };
-
 //   return (
-//     <>
-//       <div>
-//       <Container component="main" maxWidth="xs">
+//     <Container component="main" maxWidth="xs">
 //       <CssBaseline />
 //       <div className={classes.paper}>
-
 //         <Typography component="h1" variant="h5">
 //           Sign up
 //         </Typography>
-//         <form className={classes.form} onSubmit={handleSubmit} noValidate>
+//         <form className={classes.form} noValidate>
 //           <Grid container spacing={2}>
 //             <Grid item xs={12} sm={6}>
 //               <TextField
-//                 autoComplete="username"
-//                 value={name}
-//                 onChange={(e) => setName(e.currentTarget.value)}
+//                 autoComplete="fname"
+//                 name="firstName"
+//                 variant="outlined"
 //                 required
 //                 fullWidth
-//                 id="username" label="username"
-//                  variant="username"
+//                 id="firstName"
+//                 label="Username"
+//                 value={name}
+//                 onChange={(e) => setName(e.currentTarget.value)}
+//                 autoFocus
 //               />
 //             </Grid>
 //             <Grid item xs={12} sm={6}>
 //               <TextField
-//                 value={bio}
-//                 onChange={(e) => setBio(e.currentTarget.value)}
+//                 variant="outlined"
 //                 required
 //                 fullWidth
-//                 id="bio"
+//                 id="lastName"
 //                 label="Bio"
-//                 name="bio"
+//                 name="lastName"
 //                 autoComplete="lname"
+//                 value={bio}
+//                 onChange={(e) => setBio(e.currentTarget.value)}
 //               />
 //             </Grid>
 //             <Grid item xs={12}>
 //               <TextField
 //                 variant="outlined"
-//                 value={email}
-//                 onChange={(e) => setEmail(e.currentTarget.value)}
 //                 required
 //                 fullWidth
 //                 id="email"
 //                 label="Email Address"
 //                 name="email"
 //                 autoComplete="email"
+//                 value={email}
+//                 onChange={(e) => setEmail(e.currentTarget.value)}
 //               />
 //             </Grid>
 //             <Grid item xs={12}>
 //               <TextField
 //                 variant="outlined"
-//                 value={password}
-//             onChange={(e) => setPassword(e.currentTarget.value)}
 //                 required
 //                 fullWidth
 //                 name="password"
 //                 label="Password"
 //                 type="password"
 //                 id="password"
+//                 value={password}
+//                 onChange={(e) => setPassword(e.currentTarget.value)}
 //                 autoComplete="current-password"
 //               />
 //             </Grid>
 //             <Grid item xs={12}>
-//             <input type="file" onChange={handleChange} />
-//           <br />
-
+//               <input type="file" onChange={handleChange} />
+//               <br />
 //             </Grid>
 //           </Grid>
+
 //           <Button
 //             type="submit"
 //             fullWidth
 //             variant="contained"
 //             color="primary"
-//             label="Log In"
 //             className={classes.submit}
-
+//             onClick={handleUpload}
 //           >
 //             Sign Up
 //           </Button>
 //           <Grid container justify="flex-end">
 //             <Grid item>
-
+//               <Link href="#" variant="body2">
+//                 Already have an account? Sign in
+//               </Link>
 //             </Grid>
 //           </Grid>
 //         </form>
-//             <button onClick={handleUpload}>Upload Image</button>
+//         <Button onClick={handleUpload}>Upload Image</Button>
 //       </div>
 //       <Box mt={5}>
-
+//         <Copyright />
 //       </Box>
 //     </Container>
-//         {/* {error ? <div>{error}</div> : null} */}
-//         {/* <form onSubmit={handleSubmit}>
-//           <input
-//             value={name}
-//             onChange={(e) => setName(e.currentTarget.value)}
-//             type="text"
-//             placeholder="name"
-//           />
-//            <input
-//             value={bio}
-//             onChange={(e) => setBio(e.currentTarget.value)}
-//             type="text"
-//             placeholder="bio"
-//           />
-//           <input
-//             value={email}
-//             onChange={(e) => setEmail(e.currentTarget.value)}
-//             type="text"
-//             placeholder="email"
-//           />
-//           <input
-//             value={password}
-//             onChange={(e) => setPassword(e.currentTarget.value)}
-//             autoComplete="on"
-//             type="password"
-//             placeholder="password"
-//           />
-//           <input type="file" onChange={handleChange} />
-//           <br />
-//            <Dropzone handleImageChange={handleImageChange} dropzoneText={"Drop or Select Your Profile Image"}/>
-//           <button type="submit">Sign Up</button>
-//         </form>
-//         <button onClick={handleUpload}>Upload Image</button> */}
-//       </div>
-//     </>
 //   );
-// };
+// }
 
-// export default SignUpPage;
