526edea34ddbda75a347aad5bb7caf1e32736d92
DejaLFlynn <dejaflynn@pursuit.org>
2021-05-10 16:25:48 -0400

    remove duplicate

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,10 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 app.use(fileUpload())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', '*');
+//     next();
+//   });
 const users = require("./routes/users/users");
 const posts = require('./routes/posts/posts');
 const followers = require('./routes/followers/followers')
